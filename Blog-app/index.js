8d77aa7666045b65f4a2837aaacd1b0e052a7ff6
RaghavendhraR <raghulramesh843@gmail.com>
2022-11-04 08:09:56 +0530

    Todays Update

---
+++ Blog-app/index.js
@@ -0,0 +1,82 @@
+// var marks = 200;
+// const marks = 200;
+
+// if (true) {
+//   console.log(marks);
+// }
+
+// function scope() {
+//   let marks = 200;
+//   console.log(marks);
+// }
+
+// {
+//   console.log(marks);
+// }
+
+// scope();
+// console.log(marks);
+
+// Arithmetic Operators
+// var num1, num2, result;
+// num1 = 2;
+// num2 = 30;
+
+// result = num1 + num2;
+// console.log(result);
+
+// result = num1 - num2;
+// console.log(result);
+
+// result = num1 * num2;
+// console.log(result);
+
+// result = Math.floor(num2 / num1);
+// console.log(result);
+
+// result = num2 % num1;
+// console.log(result);
+
+// result = num1 ** num2;
+// console.log(result);
+
+// Comparion Operator
+// var num1, num2;
+// num1 = 30;
+// num2 = "30";
+
+// if (num1 == num2);
+// console.log("true");
+
+// console.log(num1 === num2);
+
+// Logical Operator
+// var maths, english, science, social, cutOff;
+
+// var result = "false";
+// cutOff = 80;
+// var newResult = !result;
+// console.log(newResult);
+// console.log(result);
+
+// maths = 70;
+// english = 85;
+// science = 70;
+// social = 89;
+
+// if (maths >= cutOff);
+// result = "true";
+// console.log(result);
+
+// Ternary Operator
+var marks = 40;
+grade;
+cutOff = 70;
+// if (marks >= cutOff);
+// console.log('great job');
+// else (marks >= cutOff);
+//  console.log('good jobb')
+marks >= cutOff ? console.log("great job") : console.log("Good Job");
+
+grade = marks >= cutOff ? "A" : "B";
+console.log(grade);
